e922e60ef5caef2d7f268278a8df61309eaa07f6
ElianMontenegro <elianmonte@gmail.com>
2023-11-06 16:16:16 -0300

    changes 3

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -35,18 +35,27 @@ function Home() {
 
 
   useEffect(() => {
-    // Realiza las solicitudes de tipos y Pokémon
-    Promise.all([dispatch(getAllTypes()), dispatch(getAllPokemons())])
+    // Cuando la página se carga, establece isLoading en true
+    setIsLoading(true);
+  
+    // Realiza la llamada a la API para obtener todos los tipos primero
+    dispatch(allTypes())
       .then(() => {
+        // Después de que los tipos se hayan cargado con éxito, realiza la llamada a la API para obtener todos los Pokémon
+        return dispatch(allPokemons());
+      })
+      .then(() => {
+        // Cuando ambas llamadas a la API hayan respondido con éxito, establece isLoading en false
+        setIsLoading(false);
         setAreTypesLoaded(true);
-        setIsLoading(false); // Marca que los datos se han cargado
       })
       .catch((error) => {
-        console.error("Error al obtener tipos o Pokémon:", error);
-        setIsLoading(false); // Marca que los datos se han cargado incluso en caso de error
+        // Manejo de errores si alguna de las llamadas a la API falla
+        console.error('Error al cargar los datos:', error);
+        setIsLoading(false); // Asegúrate de establecer isLoading en false incluso en caso de error
       });
   }, [dispatch]);
-
+  
   // FILTRADO POR TYPE
   const handleTypeFilter = (e) => {
     console.log("Tipo seleccionado:", e.target.value);
