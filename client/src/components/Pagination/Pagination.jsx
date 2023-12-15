a2428cb11f383869b8206532a94eb68cecb8cb93
ElianMontenegro <elianmonte@gmail.com>
2023-12-15 13:04:25 -0300

    responsive

--- client/src/components/Pagination/Pagination.jsx
+++ client/src/components/Pagination/Pagination.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import "./Pagination.css"
+import "./Pagination.css";
 
-const Pagination = ({pokemonsPerPage, allPokemons, pagination}) =>{
-    const pageNumbers = [];
+const Pagination = ({ pokemonsPerPage, allPokemons, pagination }) => {
+  const pageNumbers = [];
 
-    for(let i=1; i<=Math.ceil(allPokemons / pokemonsPerPage); i++){
-        pageNumbers.push(i)
-    }
+  for (let i = 1; i <= Math.ceil(allPokemons / pokemonsPerPage); i++) {
+    pageNumbers.push(i);
+  }
 
-    return (
-        <nav className="nav">
-          <ul className="ul">
-            {pageNumbers &&
-              pageNumbers.map((number) => (
-                <ul key={number}>
-                  <button  className="button" onClick={() => pagination(number)}>{number}</button>
-                </ul>
-              ))}
-          </ul>
-        </nav>
-      );
-}
+  return (
+    <nav className="pagination">
+      <ul className="page-list">
+        {pageNumbers &&
+          pageNumbers.map((number) => (
+            <li key={number}>
+              <button className="page-button" onClick={() => pagination(number)}>
+                {number}
+              </button>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  );
+};
+
 export default Pagination;
